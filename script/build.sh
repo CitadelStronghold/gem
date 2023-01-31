fe57637d7e669bb1862878be7ec4e7596c3d55a8
Eshnek <dylanpozarnsky@gmail.com>
2023-01-31 14:53:46 -0700

    cp

--- script/build.sh
+++ script/build.sh
@@ -10,5 +10,5 @@ mkdir -p "$(pwd)"/bin
 mkarchiso -v -w "$(pwd)"/build -o "$(pwd)"/bin $PROFILE_PATH
 
 if [[ -d "/gem" ]]; then
-    cp -r /root/bin/*.iso /gem/gem.iso
+    cp /root/bin/*.iso /gem/gem.iso
 fi
