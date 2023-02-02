781e2ffb89e6a76d54c83bc714cb8886d6f8f993
Eshnek <dylanpozarnsky@gmail.com>
2023-02-02 01:03:25 -0700

    Fix target

--- script/build.sh
+++ script/build.sh
@@ -16,7 +16,7 @@ echo "[gem] [prepare]"
 cp -r $TEMPLATE_PATH $PROFILE_PATH
 cp -r $GEM_FILES_PATH/* $PROFILE_PATH/airootfs
 
-bash gem-scripts/gem.sh $PROFILE_PATH/airootfs/opt "$(pwd)"/gem-scripts
+bash $GEM_FILES_PATH/gem-scripts/gem.sh $PROFILE_PATH/airootfs/opt "$(pwd)"/gem-scripts
 mv $PROFILE_PATH/airootfs/opt/gem-scripts $PROFILE_PATH/airootfs/opt/gem
 # git clone https://github.com/GeodeGames/gem-scripts.git $PROFILE_PATH/airootfs/opt/gem
 
